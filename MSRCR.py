9bea0689ad02aeecd3c228f8c1786f3a3a4d7e29
Grox888 <92254529+Grox888@users.noreply.github.com>
2022-01-14 00:19:21 +0800

    Update MSRCR.py

--- MSRCR.py
+++ MSRCR.py
@@ -4,12 +4,14 @@ from retinex import automatedMSRCR
 import streamlit as st
 import base64
 
-img = st.file_uploader('imge pls:')
+img = st.file_uploader('image pls:')
+gama = st.slider('gama', 0.0, 10.0, 1.0, 0.1)
 if img != None:
     img_b = base64.b64encode(img.read())
     imD = base64.b64decode(img_b)
     nparr = np.fromstring(imD, np.uint8)
     img = cv2.imdecode(nparr, cv2.IMREAD_COLOR) / 255
+    img = np.power(img, gama)
     msrcr_res = automatedMSRCR(img, [15, 80, 200])
     new_img = msrcr_res
     st.write('result of MSRCR:')
